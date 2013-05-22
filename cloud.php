e59d5e859f63fff90d8c230a54773cd02b0e04e9
Joe Kimpel <jkimpel@gmail.com>
2013-05-21 23:07:56 -0400

    Actual db query

--- cloud.php
+++ cloud.php
@@ -7,9 +7,26 @@
 	$mode = $_GET['mode'];
 	if ($mode === 'get'){
 		echo "<div>It looks like you are requesting $user-$app-$field </div>";
-	} else if ($mode === 'put'){
+	} else if ($mode === 'put'){		
 		echo "<div>It looks like you are putting $user-$app-$field value=$val</div>";
-	} else {
+	} 	else {
 		echo "<div>I don't understand...$user-$app-$field mode=$mode</div>";	
+	}
+	
+	$con = mysql_connect("localhost","852869_root","backpain");
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	mysql_select_db("backpain_zxq_cloud", $con);
+	$qstring = "select val from data where user='$user' and app='$app' and field='$field'";
+	echo "<div>Query=$qstring</div>";
+	$result = mysql_query($qstring);
+	if ($result) {
+		$row = mysql_fetch_array($result);
+		$savedVal = $row['val'];	
+		echo "<div>Returning: $savedVal</div>";
+	} else {
+		echo "<div>No results found</div>";	
 	}
 ?>
