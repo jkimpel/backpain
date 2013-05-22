3f293febfd59a40f3d8936ab8e09a7f58315ea77
Joe Kimpel <jkimpel@gmail.com>
2013-05-21 21:55:02 -0400

    Incremental update

--- cloud.php
+++ cloud.php
@@ -3,6 +3,13 @@
 	$user = $_GET['user'];
 	$app = $_GET['app'];
 	$field = $_GET['field'];
-	
-	echo "<div>It looks like you are requesting $user-$app-$field</div>";
+	$val = $_GET['val'];
+	$mode = $_GET['mode'];
+	if ($mode === 'get'){
+		echo "<div>It looks like you are requesting $user-$app-$field </div>";
+	} else if ($mode === 'put'){
+		echo "<div>It looks like you are putting $user-$app-$field value=$val</div>";
+	} else {
+		echo "<div>I don't understand...$user-$app-$field mode=$mode</div>";	
+	}
 ?>
