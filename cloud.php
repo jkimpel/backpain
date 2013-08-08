32f935dd3155488d1200489bb5e8d2542f0e5ef5
Joe Kimpel <jkimpel@gmail.com>
2013-08-07 22:27:10 -0400

    Cloud.php can now save and recall

--- cloud.php
+++ cloud.php
@@ -1,17 +1,17 @@
 <?php	
-
+	/* Joe Kimpel 8.7.13 Just for fun */
 	$user = $_GET['user'];
 	$app = $_GET['app'];
 	$field = $_GET['field'];
 	$val = $_GET['val'];
 	$mode = $_GET['mode'];
-	if ($mode === 'get'){
-		echo "<div>It looks like you are requesting $user-$app-$field </div>";
-	} else if ($mode === 'put'){		
-		echo "<div>It looks like you are putting $user-$app-$field value=$val</div>";
-	} 	else {
-		echo "<div>I don't understand...$user-$app-$field mode=$mode</div>";	
-	}
+	//if ($mode === 'get'){
+	//	echo "<div>It looks like you are requesting $user-$app-$field </div>";
+	//} else if ($mode === 'put'){		
+	//	echo "<div>It looks like you are putting $user-$app-$field value=$val</div>";
+	//} 	else {
+	//	echo "<div>I don't understand...$user-$app-$field mode=$mode</div>";	
+	//}
 	
 	$con = mysql_connect("localhost","852869_root","backpain");
 	if (!$con)
@@ -21,30 +21,39 @@
 	mysql_select_db("backpain_zxq_cloud", $con);
 	
 	if ($mode === 'put'){
-		$qstring = "select val from data where user='$user' and app='$app' and field='$field'";
-		echo "<div>Query=$qstring</div>";
+		$qstring = "select count(val) from data where user='$user' and app='$app' and field='$field'";
+		//echo "<div>Query=$qstring</div>";
 		$result = mysql_query($qstring);
+		$numResults = 0;
 		if ($result) {
 			$row = mysql_fetch_array($result);
+			$numResults = $row[0];
+		}
+		if ($numResults != 0) {
+			$qstring = "select val from data where user='$user' and app='$app' and field='$field'";
+			$result = mysql_query($qstring);
+			$row = mysql_fetch_array($result);
 			$savedVal = $row['val'];	
-			echo "<div>Replacing: $savedVal</div>";
+			//echo "<div>Replacing: $savedVal</div>";
 			$qstring = "update data set val='$val' where user='$user' and app='$app' and field='$field'";
-			echo "<div>Update=$qstring</div>";
+			//echo "<div>Update=$qstring</div>";
 			mysql_query($qstring);
 		} else {
-			echo "<div>No results found, saving new</div>";	
-			//todo insert
+			//echo "<div>No results found, saving new</div>";
+			$qstring = "insert into data (user, app, field, val) values ('$user', '$app', '$field', '$val')";
+			//echo "<div>Insert=$qstring</div>";
+			mysql_query($qstring);
 		}
 	}
 	
 	$qstring = "select val from data where user='$user' and app='$app' and field='$field'";
-	echo "<div>Query=$qstring</div>";
+	//echo "<div>Query=$qstring</div>";
 	$result = mysql_query($qstring);
 	if ($result) {
 		$row = mysql_fetch_array($result);
 		$savedVal = $row['val'];	
-		echo "<div>Returning: $savedVal</div>";
+		echo "$savedVal";
 	} else {
-		echo "<div>No results found</div>";	
+		echo "error";	
 	}
 ?>
